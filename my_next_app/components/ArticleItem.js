6934b7ef4110234c45aa832a3241a7ea61b7022c
obinnafranklinduru <obinnafranklinduru.com>
2022-11-22 17:09:43 +0100

    initial commit

--- my_next_app/components/ArticleItem.js
+++ my_next_app/components/ArticleItem.js
@@ -6,7 +6,7 @@ const ArticleItem = ({ article }) => {
     <Link href="/article/[id]" as={`/article/${article.id}`} legacyBehavior>
       <a className={Style.card}>
         <h3>{article.title} &rarr;</h3>
-        <p>{article.body}</p>
+        <p>{article.excerpt}</p>
       </a>
     </Link>
   )
